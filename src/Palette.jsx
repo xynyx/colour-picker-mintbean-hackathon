e27eb8fc4b6602bf36095c8087a058c5ede9bba9
xynyx <matt.taylr22@gmail.com>
2020-06-08 12:26:45 -0400

    Add flexbox

---
+++ src/Palette.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Palette.css';
+
+export default function Palette() {
+  return (
+    <div id="container">
+      <div class="color"></div>
+      <div class="color"></div>
+      <div class="color"></div>
+      <div class="color"></div>
+      <div class="color"></div>
+    </div>
+  )
+}
